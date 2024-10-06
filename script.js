ff7be2b890d30cd84aadf227d67f6a0251400c5e
johnathan-sewell <jonathanssewell@gmail.com>
2024-10-06 23:07:34 +0200

    bug class

--- script.js
+++ script.js
@@ -1,79 +1,96 @@
 // Game Variables
-let fps = 10;
-let fpsIntervalMs = 1000 / fps;
+const fps = 10;
+const fpsIntervalMs = 1000 / fps;
 let lastFrameTime = 0;
 
-let bugSprite;
-const bugRow = 0;
-const spriteSpace = 31;
-const firstSprite = -98 - spriteSpace;
+const playSpace = document.getElementById("playSpace");
+const bugHeight = 32;
+
+// Sprite constants
+const spriteRow = 0;
+const firstSpritePx = 127;
 const totalSprites = 8;
-let positionBottom = 0;
+const lastSpritePx = firstSpritePx + 32 * 7;
 
+// Bugs
+const bugs = [];
 
-const bug = document.getElementById("bug");
+class Bug {
+  #el;
+  #currentSpritePx;
+  #bugPositionBottomPx = 0;
+
+  constructor() {
+    this.#el = document.createElement("div");
+    this.#el.className = "bug";
+    playSpace.appendChild(this.#el);
+
+    this.#currentSpritePx = firstSpritePx;
+    this.#el.style.backgroundPositionX = this.#currentSpritePx + "px";
+    this.#el.style.backgroundPositionY = spriteRow * -32 + -3 + "px";
+  }
+
+  update() {
+    // show next sprite image
+    this.#currentSpritePx = this.#currentSpritePx + 32;
+    if (this.#currentSpritePx > lastSpritePx) {
+      this.#currentSpritePx = firstSpritePx;
+    }
+
+    // move bug up
+    if (this.#bugPositionBottomPx > playSpace.clientHeight) {
+      this.#bugPositionBottomPx = -bugHeight;
+    } else {
+      this.#bugPositionBottomPx += fps / 2;
+    }
+  }
+
+  render() {
+    // Draw your game elements here
+    this.#el.style.backgroundPositionX = this.#currentSpritePx * -1 + "px";
+    this.#el.style.bottom = this.#bugPositionBottomPx + "px";
+  }
+}
 
 // Game Initialization
 function init() {
-    // block = -98
-    bugSprite = firstSprite;
-
-    // Initialize game elements here
-    bug.style.backgroundPositionX = bugSprite + "px";
-    bug.style.backgroundPositionY = bugRow * -32 + -3 + "px";
-    bug.style.bottom = bug.style.bottom + "px";
-
+  bugs.push(new Bug());
 }
 
 // Update the game state
 function update() {
-    bugSprite = bugSprite - 32;
-    if(bugSprite < firstSprite - (totalSprites * spriteSpace)){
-       bugSprite = firstSprite;
-    }
-
-    
-    if (positionBottom === window.innerHeight) {
-        positionBottom = 0;
-    } else {
-        positionBottom+=2;
-    }
-    
-
+  bugs.forEach((bug) => bug.update());
 }
 
 // Render the game to the screen
 function render() {
-    // Draw your game elements here
-    console.log("rendered");
-    bug.style.backgroundPositionX = bugSprite + "px";
-    bug.style.bottom = positionBottom + "px";
+  bugs.forEach((bug) => bug.render());
 }
 
 // The main game loop
 function gameLoop() {
-    const now = Date.now();
-    
-    const deltaTime = now - lastFrameTime;
+  const now = Date.now();
 
-    if (deltaTime < fpsIntervalMs) {
-        console.log("Skipping");
-        return requestAnimationFrame(gameLoop);
-    }
-    // Update the game state
-    update(deltaTime);
+  const deltaTime = now - lastFrameTime;
 
-    // Render the game
-    render();
+  // Skip frame sooner than fps
+  if (deltaTime < fpsIntervalMs) {
+    return requestAnimationFrame(gameLoop);
+  }
 
-    // Set lastTime to current timestamp
-    lastFrameTime = now;
+  // Update the game state
+  update(deltaTime);
 
-    // Request the next frame
-    requestAnimationFrame(gameLoop);
+  // Render the game
+  render();
+
+  // Set lastTime to current timestamp
+  lastFrameTime = now;
+
+  // Request the next frame
+  requestAnimationFrame(gameLoop);
 }
 
 // Start the game
 init();
 requestAnimationFrame(gameLoop);
-
