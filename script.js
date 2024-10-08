9c2e101cc05ea4d66b655e0cfb668e89d341599f
johnathan-sewell <jonathanssewell@gmail.com>
2024-10-08 21:19:20 +0200

    Klaras bunny

--- script.js
+++ script.js
@@ -2,78 +2,75 @@
 const fps = 10;
 const fpsIntervalMs = 1000 / fps;
 let lastFrameTime = 0;
-
 const playSpace = document.getElementById("playSpace");
-const bugHeight = 32;
-
-// Sprite constants
-const spriteRow = 0;
-const firstSpritePx = 127;
-const totalSprites = 8;
-const lastSpritePx = firstSpritePx + 32 * 7;
 
 // Bugs
 const bugs = [];
 
 class Bug {
   #el;
+  #size = 32;
   #currentSpritePx;
-  #positionY = 0;
-  #positionX = 0;
-  #direction = "up";
+  #positionY;
+  #positionX;
+  #direction;
+  #speed;
+  #sprite;
+  #totalSprites;
 
   constructor() {
+    this.#sprite = "Pink_Monster_Idle_4.png";
+    this.#totalSprites = 4;
+    this.#positionY = 0;
+    this.#positionX = 0;
+    this.#direction = "stop";
+    this.#speed = 3;
+
     this.#el = document.createElement("div");
     this.#el.className = "bug";
     playSpace.appendChild(this.#el);
 
-    this.#currentSpritePx = firstSpritePx;
+    this.#currentSpritePx = 0;
     this.#el.style.backgroundPositionX = this.#currentSpritePx + "px";
-    this.#el.style.backgroundPositionY = spriteRow * -32 + -3 + "px";
+    this.#el.style.backgroundPositionY = 0 + "px";
 
     window.addEventListener("keydown", (key) => {
-      if (key.code === "ArrowUp") this.#direction = "up";
       if (key.code === "ArrowRight") this.#direction = "right";
-      if (key.code === "ArrowDown") this.#direction = "down";
       if (key.code === "ArrowLeft") this.#direction = "left";
+      this.#sprite = "Pink_Monster_Walk_6.png";
+      this.#totalSprites = 6;
+    });
+    window.addEventListener("keyup", (key) => {
+      if (key.code === "ArrowRight") this.#direction = "stop";
+      if (key.code === "ArrowLeft") this.#direction = "stop";
+      this.#currentSpritePx = 0;
+      this.#sprite = "Pink_Monster_Idle_4.png";
+      this.#totalSprites = 4;
     });
   }
 
   update() {
-    // show next sprite image
-    this.#currentSpritePx = this.#currentSpritePx + 32;
+    const lastSpritePx = this.#size * this.#totalSprites - 1;
+    this.#currentSpritePx = this.#currentSpritePx + this.#size;
     if (this.#currentSpritePx > lastSpritePx) {
-      this.#currentSpritePx = firstSpritePx;
+      this.#currentSpritePx = 0;
     }
 
-    if (this.#direction === "up") this.#positionY -= 4; // move bug up
-    if (this.#direction === "right") this.#positionX += 4; // move bug right
-    if (this.#direction === "down") this.#positionY += 4; // move bug down
-    if (this.#direction === "left") this.#positionX -= 4; // move bug left
+    if (this.#direction === "right") this.#positionX += this.#speed; // move right
+    if (this.#direction === "left") this.#positionX -= this.#speed; // move left
   }
 
   render() {
     // Draw your game elements here
     this.#el.style.backgroundPositionX = this.#currentSpritePx * -1 + "px";
-
-    const rotation =
-      this.#direction === "up"
-        ? Math.PI * 2
-        : this.#direction === "right"
-        ? Math.PI / 2
-        : this.#direction === "down"
-        ? Math.PI
-        : Math.PI * 1.5;
+    this.#el.style.backgroundImage = `url(./${this.#sprite})`;
 
     this.#el.style.transform =
       "translateZ(0) translate(" +
       this.#positionX +
       "px, " +
       this.#positionY +
-      "px) " +
-      "rotate(" +
-      rotation +
-      "rad)";
+      "px) scale(2)";
   }
 }
 
